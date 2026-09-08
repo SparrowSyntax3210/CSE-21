09bf4724bba4460179ce716e50920a47df14b819
Harsh Goel <sparrowsyntax3210@gmail.com>
2026-09-08 13:06:43 +0530

    Json reading

---
+++ ProductRestAPI/src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  fs.readFile("./product.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading product.json:", err);
+
+      return res.status(500).json({
+        message: "Error reading products",
+      });
+    }
+
+    try {
+      const products = JSON.parse(data);
+
+      res.json(products);
+    } catch (err) {
+      console.error("Error parsing product.json:", err);
+
+      res.status(500).json({
+        message: "Invalid JSON data",
+      });
+    }
+  });
+});
+
+module.exports = app;
